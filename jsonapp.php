191ad873b7cd0cec664e2cb4e47c1c67c98d8a37
plmcdowe <mcdowellphilip5@gmail.com>
2020-08-13 01:54:58 -0400

    Create jsonapp.php

---
+++ jsonapp.php
@@ -0,0 +1,38 @@
+<?php
+include 'androidconfig.php';
+
+// Create connection
+$conn = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
+
+if ($conn->connect_error) {
+ 
+ die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM garages";
+
+$result = $conn->query($sql);
+
+// Confirm there are results
+if ($result->num_rows >0)
+{
+ // We have results, create an array to hold the results
+        // and an array to hold the data
+ $resultArray = array();
+ $tempArray = array();
+ 
+ // Loop through each result
+ while($row = $result->fetch_object())
+ {
+ // Add each result into the results array
+ $tempArray = $row;
+     array_push($resultArray, $tempArray);
+ }
+ 
+ // Encode the array to JSON and output the results
+ echo json_encode($resultArray);
+}
+ 
+// Close connections
+mysqli_close($con);
+?>
